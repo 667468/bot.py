a026f1000c841a93ee27d4b89c0f9ce7495ddc7c
Fatih Ünsever <fatihunseverr@gmail.com>
2018-11-07 15:26:04 +0300

    bot.py: Doviz kuru komutu geliştirildi.
    Signed-off-by: Fatih Ünsever <fatihunseverr@gmail.com>

--- bot.py
+++ bot.py
@@ -8,39 +8,42 @@ token = (os.environ['BOT_TOKEN']) # Discord botu oluşturduğunuzda çıkan toke
 bot = Bot(command_prefix=bot_prefix)
 
 @bot.event
-# TODO: Türkçeleştirilebilir ama böyle de güzel
 async def on_ready():
  print("BOT: Logged as: {}".format(bot.user.name))
  print("BOT: ID: {}".format(bot.user.id))
  print("BOT: Joined {}".format(str(len(bot.servers))) + " server/s!")
  await bot.change_presence(game=Game(name='Komutlar için "!!help" yazabilirsiniz!'))
 
-# TODO: Türkçe bir komut ismi bulunca, değiştir.
-@bot.command(name='whoareu',
+@bot.command(name='bbot',
 	description="Botun hakkında bilgi verir.",
-	aliases=['??whoareu'],
+	aliases=['??bbot'],
 	pass_context=True)
-async def whoareu(ctx):
+async def bbot(ctx):
  await bot.say("Ben Fatih Ünsever tarafından yazılmış bir Discord uygulaması botuyum!")
 
 # Güncel döviz kurlarını gösterir.
 @bot.command(name='doviz',
-	description="Güncel döviz kurunu ve bitcoin fiyatını gösterir.")
-async def doviz():
- # TODO: Altın eklenebilir.
- dovizurl = "https://www.doviz.gen.tr/doviz_json.asp"
+	description="Güncel döviz kurunu ve bitcoin fiyatını gösterir.",
+	aliases=['??doviz'],
+	pass_context=True)
+async def doviz(ctx):
+ dovizurl = "http://www.floatrates.com/daily/try.json"
  response = requests.get(dovizurl)
- value_dolar_alis = response.json()['dolar']
- value_dolar_satis = response.json()['dolar2']
- value_dolar_kapanis = response.json()['ddolar2']
- value_euro_alis = response.json()['euro']
- value_euro_satis = response.json()['euro2']
- value_euro_kapanis = response.json()['deuro']
- value_son_guncellenme = response.json()['songuncellenme']
- await bot.say("Güncel Euro: " + "Alış kuru: " + value_euro_alis + ", " + "Satış kuru: " + value_euro_satis)
- await bot.say("Euro dün " + value_euro_kapanis + " seviyesinde kapanış yaptı." )
- await bot.say("Güncel Dolar: " + "Alış kuru: " + value_dolar_alis + ", " + "Satış kuru: " + value_dolar_satis)
- await bot.say("Dolar dün " + value_dolar_kapanis + " seviyesinde kapanış yaptı." )
+ value_usd_alis = response.json()['usd']['inverseRate']
+ value_eur_alis = response.json()['eur']['inverseRate']
+ value_gbp_alis = response.json()['gbp']['inverseRate']
+ value_cad_alis = response.json()['cad']['inverseRate']
+ value_chf_alis = response.json()['chf']['inverseRate']
+ value_kwd_alis = response.json()['kwd']['inverseRate']
+ value_sar_alis = response.json()['sar']['inverseRate']
+ value_son_guncellenme = response.json()['usd']['date']
+ await bot.say("Güncel 1 ABD Doları: " + "Alış fiyatı: " + str(value_usd_alis) + " TRY")
+ await bot.say("Güncel 1 Euro: " + "Alış fiyatı: " + str(value_eur_alis) + " TRY")
+ await bot.say("Güncel 1 İngiliz Sterlini: " + "Alış fiyatı: " + str(value_gbp_alis) + " TRY")
+ await bot.say("Güncel 1 Kanada Doları: " + "Alış fiyatı: " + str(value_cad_alis) + " TRY")
+ await bot.say("Güncel 1 İşviçre Frangı: " + "Alış fiyatı: " + str(value_chf_alis) + " TRY")
+ await bot.say("Güncel 1 Kuveyt Dinarı: " + "Alış fiyatı: " + str(value_kwd_alis) + " TRY")
+ await bot.say("Güncel 1 S. Arabistan Riyali: " + "Alış fiyatı: " + str(value_sar_alis) + " TRY")
  await bot.say("Son güncellenme: " + value_son_guncellenme)
  # TODO: USD değil de TRY olabilir.
  # Bitcoin fiyatı
