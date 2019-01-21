299a5a50014f81cda46414e6a2c05ddcb0078d46
Fatih Ünsever <fatihunseverr@gmail.com>
2019-01-21 18:07:34 +0300

    bot.py: Komutlar duzenlendi
    Breaking Bad dizisinden replik ureten komut silindi.
    Randompass komutu artik istenilen sayida sifre uretiyor.
    
    Signed-off-by: Fatih Ünsever <fatihunseverr@gmail.com>

--- bot.py
+++ bot.py
@@ -4,68 +4,63 @@ import random
 import string
 import requests
 
-token = (os.environ['BOT_TOKEN']) # Telegram botunu oluşturduğunuzda çıkan tokeni 'BOT_TOKEN' kısmına ekleyin.
+token = (os.environ['BOT_TOKEN']) # Telegram botunu oluşturduğunuzda ki token.
 bot = telebot.TeleBot(token)
 
-@bot.message_handler(commands=['help'])
+@bot.message_handler(commands=['help', 'start'])
+@bot.edited_message_handler(commands=['help', 'start'])
 def help(message):
-	info = ('/randompass - Rastgele, harf ve rakamlardan oluşan, 11 haneli şifre oluşturur.\n'
+	info = ('/randompass - Rastgele, harf ve rakamlardan oluşan, istenilen sayıda şifre oluşturur.\n'
 		'/randomusername - Rastgele kullanıcı adı üretir.\n'
-		'/flipcoin - Yazı-tura atar.\n'
 		'/randomcat - Rastgele kedi fotoğrafı gönderir.\n'
 		'/randomdog - Rastgele köpek fotoğrafı gönderir.\n'
-		'/randombb - Rastgele Breaking Bad dizisinden replik gönderir.\n'
+		'/flipcoin - Yazı-tura atar.'
 		)
 	bot.reply_to(message, info)
 
-@bot.message_handler(content_types=['text'])
-@bot.edited_message_handler(content_types=['text'])
-def text(message):
-	if '/start' in message.text:
-		welcome = ('Hoşgeldiniz! '
-			'Komutlar için "/help" yazabilirsiniz!\n'
-			)
-		bot.reply_to(message, welcome)
+@bot.message_handler(commands=['randompass'])
+@bot.edited_message_handler(commands=['randompass'])
+def randompass(message):
+	try:
+		passlen = int(message.text.split(' ')[1])
+		if passlen < 6:
+			bot.send_message(message.chat.id, 'Güvenliğiniz için minimum 6 haneli şifre oluşturun!')
+	except IndexError:
+		bot.send_message(message.chat.id, 'Örnek kullanım: /randompass 9')
 		return
+	passgenerator = [random.choice(string.ascii_letters+string.digits) for i in range(passlen)]
+	passgenerator = ("".join(passgenerator))
+	bot.send_message(message.chat.id, passgenerator)
 
-	if '/randompass' in message.text:
-		passlen = 11 #TODO: İstenilen sayıya göre ayarla!
-		passgenerator = [random.choice(string.ascii_letters+string.digits) for i in range(passlen)]
-		passgenerator = ("".join(passgenerator))
-		bot.send_message(message.chat.id, passgenerator)
-		return
+@bot.message_handler(commands=['randomusername'])
+@bot.edited_message_handler(commands=['randomusername'])
+def randomusername(message):
+	randomusernameurl = "https://api.randomuser.me/"
+	response = requests.get(randomusernameurl)
+	value_random_username = response.json()['results'][0]['login']['username']
+	bot.send_message(message.chat.id, value_random_username)
 
-	if '/randomusername' in message.text:
-		randomusernameurl = "https://api.randomuser.me/"
-		response = requests.get(randomusernameurl)
-		value_random_username = response.json()['results'][0]['login']['username']
-		bot.send_message(message.chat.id, value_random_username)
+@bot.message_handler(commands=['randomcat'])
+@bot.edited_message_handler(commands=['randomcat'])
+def randomcat(message):
+	randomcaturl = "https://api.thecatapi.com/v1/images/search?format=json"
+	response = requests.get(randomcaturl)
+	value_random_cat = response.json()[0]['url']
+	bot.send_photo(message.chat.id, value_random_cat)
 
-	if '/flipcoin' in message.text:
-		liste = ['yazı', 'tura']
-		yazitura = [random.choice(liste)]
-		bot.send_message(message.chat.id, yazitura)
-		return
+@bot.message_handler(commands=['randomdog'])
+@bot.edited_message_handler(commands=['randomdog'])
+def randomdog(message):
+	randomdogurl = "https://dog.ceo/api/breeds/image/random"
+	response = requests.get(randomdogurl)
+	value_random_dog = response.json()['message']
+	bot.send_photo(message.chat.id, value_random_dog)
 
-	if '/randomcat' in message.text:
-		randomcaturl = "https://api.thecatapi.com/v1/images/search?format=json"
-		response = requests.get(randomcaturl)
-		value_random_cat = response.json()[0]['url']
-		bot.send_photo(message.chat.id, value_random_cat)
-		return
-
-	if '/randomdog' in message.text:
-		randomdogurl = "https://dog.ceo/api/breeds/image/random"
-		response = requests.get(randomdogurl)
-		value_random_dog = response.json()['message']
-		bot.send_photo(message.chat.id, value_random_dog)
-		return
-
-	if '/randombb' in message.text:
-		randombburl = "https://breaking-bad-quotes.herokuapp.com/v1/quotes"
-		response = requests.get(randombburl)
-		value_random_bbquote = response.json()[0]['quote']
-		value_random_bbauthor = response.json()[0]['author']
-		bot.send_message(message.chat.id, value_random_bbquote + " - " + value_random_bbauthor)
+@bot.message_handler(commands=['flipcoin'])
+@bot.edited_message_handler(commands=['flipcoin'])
+def flipcoin(message):
+	liste = ['Yazı', 'Tura']
+	yazitura = [random.choice(liste)]
+	bot.send_message(message.chat.id, yazitura)
 
 bot.polling(none_stop=True, interval=0, timeout=3)
